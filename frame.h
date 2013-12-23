95c003d220c4cbffaca6676fa6d8968a8fa94873
EdWard680 <ezeward4@gmail.com>
2013-12-22 21:34:14 -0500

    Frame: Adds interface

--- frame.h
+++ frame.h
@@ -3,7 +3,28 @@
 
 #include "pinset.h"
 
-
+class frame
+{
+public:
+  enum {FIRST=0, SECOND, COMPLETE} Stage_t;
+  enum {ROLL=0, SPARE, BONUS};
+  
+private:
+  Stage_t stage;
+  bool mark;
+  PinSet pins;
+  PINS *roll;
+  mutable PINS total;
+  
+public:
+  const Stage_t getStage() const {return stage;};
+  const PINS GetTotal() const {return mark? (roll[ROLL] + roll[SPARE] + roll[BONUS]):(roll[ROLL] + roll[SPARE]);};
+  const PinSet GetPinSet() const {return pins;};
+  void Roll(PINS * const n);
+  
+  frame(roll * const init=NULL): roll(init) {};
+  
+};
 
 
 
